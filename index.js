012714191a0e5f41cee1728c43e954972a168864
Nils Matic <matic1909@gmail.com>
2023-05-03 17:59:01 +0200

    added playRound function to determine winner of a round

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3);
   console.log(choices[randomIndex]);
 }
+
+function playRound(playerChoice, computerChoice) {
+  // determine who won based on player and computer choice. -1 -> computer won, 1 -> player won, 0 -> draw
+  if (playerChoice === 'rock') {
+    if (computerChoice === 'paper') return -1;
+    if (computerChoice === 'scissors') return 1;
+    return 0;
+  } else if (playerChoice === 'paper') {
+    if (computerChoice === 'scissors') return -1;
+    if (computerChoice === 'rock') return 1;
+    return 0;
+  } else {
+    if (computerChoice === 'rock') return -1;
+    if (computerChoice === 'paper') return 1;
+    return 0;
+  }
+}
