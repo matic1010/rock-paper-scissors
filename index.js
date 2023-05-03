8fd0c6f7383ab0674ac75ef24a1027b1a3582247
Nils Matic <matic1909@gmail.com>
2023-05-04 00:30:37 +0200

    added styling, check for winner, modal and reset button

--- index.js
+++ index.js
@@ -1,16 +1,88 @@
 const choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
 
+const playerChoiceDisplay = document.querySelector('#player-choice');
+const computerChoiceDisplay = document.querySelector('#computer-choice');
+const playerScoreDipslay = document.querySelector('#player-score');
+const computerScoreDisplay = document.querySelector('#computer-score');
 const buttons = document.querySelectorAll('.selection-button');
+const modal = document.querySelector('#modal');
+const modalResult = document.querySelector('.end-result');
+const resetButton = document.querySelector('.reset');
+
 buttons.forEach((button) =>
   button.addEventListener('click', (e) => {
     const playerChoice = e.target.id;
-    console.log(playRound(playerChoice, getComputerChoice()));
+    handleClick(playerChoice);
   })
 );
+
+resetButton.addEventListener('click', reset);
 
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3);
   return choices[randomIndex];
+}
+
+function handleClick(playerChoice) {
+  const computerChoice = getComputerChoice();
+  updateSelectionDisplay(playerChoice, computerChoice);
+  const result = playRound(playerChoice, computerChoice);
+  updateScore(result);
+  checkForWin();
+}
+
+function checkForWin() {
+  if (round >= 5) {
+    if (playerScore > computerScore) modalResult.textContent = 'You win!';
+    else if (playerScore < computerScore) modalResult.textContent = 'You lose!';
+    else modalResult.textContent = "It's a tie";
+    endGame();
+  }
+}
+
+function endGame() {
+  buttons.forEach((button) => {
+    button.setAttribute('disabled', 'true');
+  });
+  setTimeout(function () {
+    modal.style.display = 'block';
+  }, 1000);
+}
+
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+  round = 0;
+  buttons.forEach((button) => {
+    button.removeAttribute('disabled', 'false');
+  });
+  modal.style.display = 'none';
+}
+
+function choiceToEmoji(choice) {
+  if (choice === 'rock') return '🪨';
+  if (choice === 'paper') return '🧻';
+  if (choice === 'scissors') return '✂️';
+}
+
+function updateSelectionDisplay(playerChoice, computerChoice) {
+  playerChoiceDisplay.textContent = choiceToEmoji(playerChoice);
+  computerChoiceDisplay.textContent = choiceToEmoji(computerChoice);
+}
+
+function updateScoreDisplay() {
+  playerScoreDipslay.textContent = playerScore;
+  computerScoreDisplay.textContent = computerScore;
+}
+
+function updateScore(result) {
+  if (result === -1) computerScore++;
+  if (result === 1) playerScore++;
+  round++;
+  updateScoreDisplay();
 }
 
 function playRound(playerChoice, computerChoice) {
@@ -28,10 +100,4 @@ function playRound(playerChoice, computerChoice) {
     if (computerChoice === 'paper') return 1;
     return 0;
   }
-}
-
-function generateResultString(result) {
-  if (result === -1) return 'Computer wins!';
-  else if (result === 1) return 'Player wins!';
-  else return "It's a draw!";
 }
